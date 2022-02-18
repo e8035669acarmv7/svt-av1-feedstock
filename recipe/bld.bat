ea0d697be33c24e2d661ffb5ad5dd521b5741a0a
Matthias Diener <mdiener@illinois.edu>
2022-02-18 16:37:26 -0600

    add win64 build

---
+++ recipe/bld.bat
@@ -0,0 +1,17 @@
+@echo on
+
+cd ..\Build
+
+cmake -G "NMake Makefiles"       ^ 
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DBUILD_SHARED_LIBS=ON     ^ 
+      -DNATIVE=OFF               ^
+      ..
+
+if errorlevel 1 exit 1
+nmake
+
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
