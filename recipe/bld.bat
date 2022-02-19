76e7771297c0295111a7121aa7d24e8d2b0b44cb
Matthias Diener <matthias.diener@gmail.com>
2022-02-18 18:45:36 -0600

    path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,10 +2,12 @@
 
 cd Build
 
-cmake -G "NMake Makefiles"       ^
-      -DCMAKE_BUILD_TYPE=Release ^
-      -DBUILD_SHARED_LIBS=ON     ^
-      -DNATIVE=OFF               ^
+cmake -G "NMake Makefiles"                           ^
+      -DCMAKE_BUILD_TYPE=Release                     ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_LIBDIR="lib"                   ^
+      -DBUILD_SHARED_LIBS=ON                         ^
+      -DNATIVE=OFF                                   ^
       ..
 
 if errorlevel 1 exit 1
